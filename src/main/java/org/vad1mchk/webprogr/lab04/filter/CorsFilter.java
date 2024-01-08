9d1d7b1d2036daea1d676d2dd3575f9705aa1e28
vadim <snusmumrike2@gmail.com>
2024-01-08 19:54:46 +0300

    ...

--- src/main/java/org/vad1mchk/webprogr/lab04/filter/CorsFilter.java
+++ src/main/java/org/vad1mchk/webprogr/lab04/filter/CorsFilter.java
@@ -5,15 +5,13 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.ext.Provider;
 
-import java.io.IOException;
-
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://se.ifmo.ru");
+                       ContainerResponseContext responseContext) {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Headers",
                 "origin, content-type, accept, authorization");
